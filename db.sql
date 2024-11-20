2cceff5460997053d834c1496c88b8e497e078d8
Caotrieu78 <caotrieu0708@gmail.com>
2024-11-20 21:34:11 +0700

    trieu push db

---
+++ db.sql
@@ -0,0 +1,283 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: customermanagementdb
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `CustomerID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` varchar(50) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `ClassificationID` int DEFAULT NULL,
+  `UserID` int DEFAULT NULL,
+  `ProjectID` int DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CustomerID`),
+  KEY `ClassificationID` (`ClassificationID`),
+  KEY `UserID` (`UserID`),
+  KEY `ProjectID` (`ProjectID`),
+  CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`ClassificationID`) REFERENCES `customer_classification` (`ClassificationID`),
+  CONSTRAINT `customer_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  CONSTRAINT `customer_ibfk_3` FOREIGN KEY (`ProjectID`) REFERENCES `project` (`ProjectID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_classification`
+--
+
+DROP TABLE IF EXISTS `customer_classification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_classification` (
+  `ClassificationID` int NOT NULL AUTO_INCREMENT,
+  `ClassificationName` varchar(100) NOT NULL,
+  PRIMARY KEY (`ClassificationID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_classification`
+--
+
+LOCK TABLES `customer_classification` WRITE;
+/*!40000 ALTER TABLE `customer_classification` DISABLE KEYS */;
+INSERT INTO `customer_classification` VALUES (1,'VIP'),(2,'Normal'),(3,'Potential');
+/*!40000 ALTER TABLE `customer_classification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event` (
+  `EventID` int NOT NULL AUTO_INCREMENT,
+  `CustomerID` int DEFAULT NULL,
+  `ProjectID` int DEFAULT NULL,
+  `UserID` int DEFAULT NULL,
+  `EventTypeID` int DEFAULT NULL,
+  `EventDate` date NOT NULL,
+  `Description` text,
+  `ReminderDate` date DEFAULT NULL,
+  `ReminderSent` tinyint(1) DEFAULT '0',
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`EventID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `ProjectID` (`ProjectID`),
+  KEY `UserID` (`UserID`),
+  KEY `EventTypeID` (`EventTypeID`),
+  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
+  CONSTRAINT `event_ibfk_2` FOREIGN KEY (`ProjectID`) REFERENCES `project` (`ProjectID`),
+  CONSTRAINT `event_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  CONSTRAINT `event_ibfk_4` FOREIGN KEY (`EventTypeID`) REFERENCES `event_type` (`EventTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event_type`
+--
+
+DROP TABLE IF EXISTS `event_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event_type` (
+  `EventTypeID` int NOT NULL AUTO_INCREMENT,
+  `EventTypeName` varchar(100) NOT NULL,
+  PRIMARY KEY (`EventTypeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event_type`
+--
+
+LOCK TABLES `event_type` WRITE;
+/*!40000 ALTER TABLE `event_type` DISABLE KEYS */;
+INSERT INTO `event_type` VALUES (1,'Meeting'),(2,'Contract Signing'),(3,'Payment Due'),(4,'Project Review');
+/*!40000 ALTER TABLE `event_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `PaymentID` int NOT NULL AUTO_INCREMENT,
+  `CustomerID` int DEFAULT NULL,
+  `ProjectID` int DEFAULT NULL,
+  `InstallmentNumber` int DEFAULT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `PaymentDate` date DEFAULT NULL,
+  `PaymentStatus` enum('Pending','Paid','Failed') DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`PaymentID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `ProjectID` (`ProjectID`),
+  CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
+  CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`ProjectID`) REFERENCES `project` (`ProjectID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
+  `ProjectID` int NOT NULL AUTO_INCREMENT,
+  `CustomerID` int DEFAULT NULL,
+  `UserID` int DEFAULT NULL,
+  `ProjectName` varchar(255) NOT NULL,
+  `Description` text,
+  `StartDate` date DEFAULT NULL,
+  `EndDate` date DEFAULT NULL,
+  `Status` enum('Ongoing','Completed','Accepted_NotPaid','Canceled') NOT NULL,
+  `ProjectTypeID` int DEFAULT NULL,
+  `TotalAmount` decimal(10,2) DEFAULT NULL,
+  `PaidAmount` decimal(10,2) DEFAULT '0.00',
+  `RemainingAmount` decimal(10,2) GENERATED ALWAYS AS ((`TotalAmount` - `PaidAmount`)) STORED,
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ProjectID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `UserID` (`UserID`),
+  KEY `ProjectTypeID` (`ProjectTypeID`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
+  CONSTRAINT `project_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`),
+  CONSTRAINT `project_ibfk_3` FOREIGN KEY (`ProjectTypeID`) REFERENCES `project_type` (`ProjectTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_type`
+--
+
+DROP TABLE IF EXISTS `project_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project_type` (
+  `ProjectTypeID` int NOT NULL AUTO_INCREMENT,
+  `TypeName` varchar(100) NOT NULL,
+  PRIMARY KEY (`ProjectTypeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_type`
+--
+
+LOCK TABLES `project_type` WRITE;
+/*!40000 ALTER TABLE `project_type` DISABLE KEYS */;
+INSERT INTO `project_type` VALUES (1,'Resort'),(2,'High-Rise'),(3,'Luxury Housing'),(4,'Residential Area'),(5,'Factory');
+/*!40000 ALTER TABLE `project_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `FullName` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Role` enum('Admin','Staff Manager') NOT NULL,
+  `Avatar` varchar(255) DEFAULT NULL,
+  `CreatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `Username` (`Username`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-17 21:36:04
